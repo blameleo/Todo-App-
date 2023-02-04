633cf19f94ee64dd99eef44627275ed04a1db63c
blameleo <leonardgre@gmail.com>
2023-02-04 19:28:50 +0000

    created the components

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import Header from "./Header";
+import Input from "./Input";
+import Todo from "./Todo";
+
+
+
+const App = () => {
+    return ( 
+        <div>
+            <Header />
+            <Input />
+            <Todo />
+
+        </div>
+     );
+}
+ 
+export default App;
